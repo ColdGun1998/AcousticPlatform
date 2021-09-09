4cbccff0d3cedc10ac8c342b050fb83e34270adc
ColdGun1998 <1744849839@qq.com>
2021-09-09 13:02:47 +0800

    使用vue3加载蜂鸟地图

--- src/components/FengMap.vue
+++ src/components/FengMap.vue
@@ -8,8 +8,50 @@ import '../fengmap/fengmap.control.min.js' // 控件
 import '../fengmap/fengmap.analyzer.min.js' // 路径分析类
 import '../fengmap/fengmap.navi.min.js' // 导航类
 import { ElLoading } from 'element-plus'
-var map = null
-var loadingInstance = null
+import { onMounted, watch, onUnmounted } from 'vue'
+// fengMap有关逻辑
+const useFengMapEffect = () => {
+  let map = null
+  let loadingInstance = null
+  // 初始化加载动画
+  const initLoading = () => {
+    const options = {
+      target: document.getElementById('fengMap'),
+      text: '加载蜂鸟地图中',
+      fullscreen: false
+    }
+    loadingInstance = ElLoading.service(options)
+  }
+  // 打开地图
+  const openMap = (fmapID, appName, appKey) => {
+    initLoading()
+    map = new fengmap.FMMap({
+      container: document.getElementById('fengMap'), // 渲染dom
+      appName: appName,
+      key: appKey
+    })
+    map.on('loadComplete', function (event) {
+      loadingInstance.close()
+    })
+    map.openMapById(fmapID)
+  }
+  // 销毁地图
+  const disposeMap = () => {
+    if (map != null) {
+      map.dispose()
+    }
+  }
+  return {
+    openMap,
+    disposeMap
+  }
+}
+// 定时获取最新定位数据的逻辑
+// const useLocationEffect = () => {
+//   return {
+//     0
+//   }
+// }
 export default {
   name: 'FengMap',
   props: {
@@ -17,42 +59,18 @@ export default {
     appName: String,
     appKey: String
   },
-  mounted () {
-    this.openMap(this.fmapID, this.appName, this.appKey)
-  },
-  watch: {
-    fmapID: function (newVal, oldVal) {
-      if (map != null) {
-        map.dispose()
-      }
-      this.openMap(this.fmapID, this.appName, this.appKey)
-    }
-  },
-  methods: {
-    initLoading () {
-      const options = {
-        target: document.getElementById('fengMap'),
-        text: '加载蜂鸟地图中',
-        fullscreen: false
-      }
-      loadingInstance = ElLoading.service(options)
-    },
-    openMap (fmapID, appName, appKey) {
-      this.initLoading()
-      map = new fengmap.FMMap({
-        container: document.getElementById('fengMap'), // 渲染dom
-        // 必要，地图应用名称，通过蜂鸟云后台创建
-        appName: appName,
-        // 必要，地图应用密钥，通过蜂鸟云后台获取
-        key: appKey
-      })
-      map.on('loadComplete', function (event) {
-        loadingInstance.close()
-      })
-      // 打开Fengmap服务器的地图数据和主题
-      map.openMapById(fmapID, () => {
-      })
-    }
+  setup (props) {
+    const { openMap, disposeMap } = useFengMapEffect()
+    onMounted(() => {
+      openMap(props.fmapID, props.appName, props.appKey)
+    })
+    onUnmounted(() => {
+      disposeMap()
+    })
+    watch(() => props, () => {
+      disposeMap()
+      openMap(props.fmapID, props.appName, props.appKey)
+    }, { deep: true })
   }
 }
 </script>
